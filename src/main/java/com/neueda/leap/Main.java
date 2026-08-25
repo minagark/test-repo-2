e937c2b8d6a2ac9bb4a7fb2bada1a1f68b8f7dcc
Money Man <money@man.com>
2026-08-25 17:18:14 +0000

    Mihir

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
         System.out.println("Hi Again");
-        System.out.println("Hi Again v2");
+        System.out.println("H");
     }
 }
