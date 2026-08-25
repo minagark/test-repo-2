b370a18ec8f8cdb4abc452a680fc0105b7b83ded
Mihir Nagarkatti <mihir.nagarkatti@gmail.com>
2026-08-25 16:53:16 +0000

    feat: add another print statement

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
         System.out.println("Hi Again");
+        System.out.println("Hi Again v2");
     }
 }
